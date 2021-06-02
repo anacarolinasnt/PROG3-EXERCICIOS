87d2eeb68c2472cd02b7f1ffbb21e8ff4c33c84c
Ana Carolina Santos <ana.carolinasnt@hotmail.com>
2021-06-01 22:50:18 -0300

    Aula03
    Conteúdo do dia 27/05/2021 e Postagem da Atividade

---
+++ Aula03/scripts.js
@@ -0,0 +1,10 @@
+function funcOlaImagem(img){
+    switch(img){
+        case 1:
+            alert("Imagem Logo!!");
+            break;
+        case 2:
+            alert('Imagem JavaScript!!');
+            break;
+    }
+ }
