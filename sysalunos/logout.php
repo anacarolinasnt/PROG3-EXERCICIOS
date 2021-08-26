d2d9775cf90b54e9933aed2e2753897879252101
Ana Carolina Santos <ana.carolinasnt@hotmail.com>
2021-08-26 10:33:54 -0300

    Revert "alunosAPI"
    This reverts commit e9687d729c46d2dbfd3d2252264b5898837697a0.

--- sysalunos/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-
-    session_destroy(); //limpa a sessão
-
-    header("location: index.php");
-
-?>
--- sysalunos/variaveis.php
+++
@@ -1,7 +0,0 @@
-<?php
-    //controle de variáveis Globais
-    //Que podemos estar utilizando em todas as páginas (sistema)
-    global $conexao_bd;
-    global $nome_usuario;
-    global $nome_aluno;
-?>
